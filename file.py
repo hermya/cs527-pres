346cb6ea25eea85ea50ca72cef414bea313fb587
Heramb Joshi <herambku@gmail.com>
2024-10-28 20:04:50 -0500

    Imported my_deque in demonstration file and replicated usage

--- file.py
+++ file.py
@@ -1,5 +1,5 @@
 from collections import deque
-
+from my_collections.my_deque import Deque as myDeque
 my_cache = deque()
 
 my_cache.append(1)
@@ -11,3 +11,14 @@ my_cache.appendleft(-2)
 my_cache.appendleft(-3)
 
 print(my_cache)
+
+my_cache = myDeque()
+my_cache.addRight(Node(1))
+my_cache.addRight(Node(2))
+my_cache.addRight(Node(3))
+
+my_cache.addLeft(Node(-1))
+my_cache.addLeft(Node(-2))
+my_cache.addLeft(Node(-3))
+
+my_cache.print()
